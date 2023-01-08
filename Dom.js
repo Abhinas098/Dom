25c8273f4edaafc03b0aa508a453b2df41c463a2
Abhinas <abhinasdash143@gmail.com>
2023-01-08 22:42:30 +0530

    Dom-Task-1

---
+++ Dom.js
@@ -0,0 +1,25 @@
+// console.dir(document);
+
+// Examine the  Document object.
+// console.log(document.domain)
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123 //tochange the title
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[9]);
+// // document.all[10].textContent = 'hello'; //change title because it in index 10
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+
+// // GETELEMENTBYID//
+// var headTitle = document.getElementById('header-title');
+// // console.log(headTitle);
+// // headTitle.textContent = "hello"
+// // headTitle.innerText = "godd byy"
+// // headTitle.innerHTML ="<h1>Hello</H2>"
+// headTitle = 
