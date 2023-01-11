19b224a600b39c3e5cc4d277807e6f642a54a75e
Abhinas <abhinasdash143@gmail.com>
2023-01-11 19:39:48 +0530

    stringify

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 // MOUSEOVER:-
 const btn = document.querySelector('.btn');
-btn.addEventListener('mouseover', (e)=>{
+btn.addEventListener('mouseover', (e) => {
     e.preventDefault();
-    document.querySelector('#form').style.background="lightblue"
-    document.querySelector('body').style.background='black'
-    document.querySelector('header').style.background='lightgray'
-    document.querySelector('header').style.color='black'});
-    
+    document.querySelector('#form').style.background = "lightblue"
+    document.querySelector('body').style.background = 'black'
+    document.querySelector('header').style.background = 'lightgray'
+    document.querySelector('header').style.color = 'black'
+});
+
 // MOUSEOUT:-
 const btn2 = document.querySelector('.btn');
-btn2.addEventListener('mouseout', (e)=>{
+btn2.addEventListener('mouseout', (e) => {
     e.preventDefault();
-    document.querySelector('#form').style.background="lightgreen"
-    document.querySelector('body').style.background='gray'
-    document.querySelector('header').style.background='black'
-    document.querySelector('header').style.color='white'});
+    document.querySelector('#form').style.background = "lightgreen"
+    document.querySelector('body').style.background = 'gray'
+    document.querySelector('header').style.background = 'black'
+    document.querySelector('header').style.color = 'white'
+});
 
 
 // console.log(document.getElementById('form').value)
@@ -24,24 +26,28 @@ const email = document.querySelector('#mail');
 const msg = document.querySelector('.msg');
 const ul = document.querySelector('#users');
 
-form.addEventListener('submit',onSubmit);
+form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
     e.preventDefault()
 
-    if(namee.value || email.value){
+    if (namee.value || email.value) {
+        //Task-1
+        // localStorage.setItem('Name:',namee.value)
+        // localStorage.setItem('E-mail:',email.value)
+        // console.log('Name:',localStorage.getItem('Name:'))
+        // console.log('E-mail:',localStorage.getItem('E-mail:'))
 
-        localStorage.setItem('Name:',namee.value)
-        localStorage.setItem('E-mail:',email.value)
-        console.log('Name:',localStorage.getItem('Name:'))
-        console.log('E-mail:',localStorage.getItem('E-mail:'))
 
-
+        // Task-2
+        let strobj=JSON.stringify(obj);
+        localStorage.setItem('obj:',strobj)
+        let obobj = JSON.parse(localStorage.getItem('obj'))
     }
 
-else if (namee.value === '' || email.value===''){
+    else if (namee.value === '' || email.value === '') {
         msg.classList.add('error')
-        msg.innerHTML ='please enter all fields'
-        setTimeout(()=> msg.remove(), 3000)
-}
+        msg.innerHTML = 'please enter all fields'
+        setTimeout(() => msg.remove(), 3000)
+    }
 }
